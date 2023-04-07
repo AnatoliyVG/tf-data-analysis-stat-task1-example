0b8bc9cd1c4b8e825cf1e28683cd194e84e103ab
AnatoliyVG <130177143+AnatoliyVG@users.noreply.github.com>
2023-04-07 22:10:56 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,11 +1,17 @@
 import pandas as pd
 import numpy as np
 
+chat_id = 1399245853 
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
 
-def solution(x: np.array) -> float:
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    return x.mean() # Ваш ответ
+def estimate_acceleration(path_measurements):
+    median = np.median(path_measurements) # оценка медианы
+    acceleration = 2 * median / 49 # формула для коэффициента ускорения
+    return acceleration
+
+    
+     
+
+path_measurements = np.array([10, 12, 9, 11, 10.5])
+acceleration = estimate_acceleration(path_measurements)
+print(acceleration)
